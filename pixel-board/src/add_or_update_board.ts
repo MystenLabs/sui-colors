698760c81d1864e166e38d2168b76f0588376e1a
“George <george.digkas@mystenlabs.com>
2023-10-17 02:35:18 -0600

    add ts code for add_or_update_board

---
+++ pixel-board/src/add_or_update_board.ts
@@ -0,0 +1,44 @@
+import { SuiClient } from "@mysten/sui.js/client";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
+const phrase = process.env.ADMIN_PHRASE;
+const fullnode = process.env.FULLNODE!;
+const keypair = Ed25519Keypair.deriveKeypair(phrase!);
+const client = new SuiClient({
+  url: fullnode,
+});
+
+const packageId = process.env.PACKAGE_ID;
+
+let transactionBlock = new TransactionBlock();
+
+transactionBlock.moveCall({
+  target: `${packageId}::board::add_or_update_board`,
+  arguments: [
+    transactionBlock.object(
+      "0x5454237f232a31874fc5fd2d2128d46cd43f12146b7af2ccc6615e16e56409c0"
+    ),
+    transactionBlock.pure(0),
+    transactionBlock.pure(0),
+    transactionBlock.pure("000000"),
+  ],
+});
+
+transactionBlock.setGasBudget(50000000000);
+client
+  .signAndExecuteTransactionBlock({
+    transactionBlock,
+    requestType: "WaitForLocalExecution",
+    options: {
+      showObjectChanges: true,
+      showEffects: true,
+    },
+    signer: keypair,
+  })
+  .then((result) => {
+    console.log(result);
+  });
