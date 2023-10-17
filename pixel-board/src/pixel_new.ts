bc700c25f28284b0cb47adf86dad45d1dc769645
“George <george.digkas@mystenlabs.com>
2023-10-17 02:08:14 -0600

    add ts code to create a new pixel

--- pixel-board/src/pixel_new.ts
+++ pixel-board/src/pixel_new.ts
@@ -8,6 +8,7 @@ dotenv.config({ path: "../../.env" });
 const phrase = process.env.ADMIN_PHRASE;
 const fullnode = process.env.FULLNODE!;
 const keypair = Ed25519Keypair.deriveKeypair(phrase!);
+const adminAddress = keypair.getPublicKey().toSuiAddress();
 const client = new SuiClient({
   url: fullnode,
 });
@@ -17,7 +18,8 @@ const packageId = process.env.PACKAGE_ID;
 let transactionBlock = new TransactionBlock();
 
 transactionBlock.moveCall({
-  target: `${packageId}::pixel_board::new`,
+  target: `${packageId}::board::create_board`,
+  arguments: [transactionBlock.pure(100)],
 });
 
 transactionBlock.setGasBudget(10000000);
