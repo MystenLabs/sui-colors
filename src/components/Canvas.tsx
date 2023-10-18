666beff06a2a08b273d00bdc8f56ee35b10dedfe
Phong Lam <phong@mystenlabs.com>
2023-10-17 21:34:05 -0600

    add eraser and size

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Container } from "@radix-ui/themes";
+import {Button, Container, Flex, RadioGroup, Text} from "@radix-ui/themes";
 import { HexColorPicker } from "react-colorful";
 import { TransactionBlock } from "@mysten/sui.js/transactions";
 import {
@@ -10,9 +10,45 @@ import ColorGrid from "./ColorGrid";
 import { useGetCanvas } from "../useGetCanvas";
 import { SuiObjectData } from "@mysten/sui.js/client";
 
+const cursorSizes = {
+    Small: 1,
+    Medium: 2,
+    Large: 3,
+}
+
+enum CursorSizes {
+    Small = 'Small',
+    Medium = 'Medium',
+    Large = 'Large',
+}
+
+const cursorSelection: CursorSizes[] = [CursorSizes.Small, CursorSizes.Medium, CursorSizes.Large];
+
+function getCoords(size: CursorSizes, [x, y]: [number, number]) {
+  if (size === CursorSizes.Small) {
+    return [[x, y]];
+  }
+
+  const sizeToNum = cursorSizes[size];
+
+  const coords = [];
+  const gridSize = sizeToNum * 2 - 1
+  const halfGridSize = Math.floor(gridSize / 2);
+
+  for (let i = -halfGridSize; i <= halfGridSize; i++) {
+    for (let j = -halfGridSize; j <= halfGridSize; j++) {
+      coords.push([x + i, y + j]);
+    }
+  }
+
+  return coords;
+}
+
 const Canvas: React.FC = () => {
   const client = useSuiClient();
 
+  const [deltas, setDeltas] = useState<Set<string>>(new Set());
+  const [cursorSize, setCursorSize] = useState(CursorSizes.Small);
   const [color, setColor] = useState("#aabbcc");
   const { mutate: signAndExecuteTransactionBlock } =
     useSignAndExecuteTransactionBlock();
@@ -26,29 +62,24 @@ const Canvas: React.FC = () => {
   // Dragging Mouse
   const [isDragging, setIsDragging] = useState(false);
 
-  const handleMouseDown = (_e: React.MouseEvent) => {
-    if (!isDragging) {
-      setIsDragging(true);
-    }
-  };
-
   const handleMouseMove = (
     _e: React.MouseEvent,
     rowIndex: number,
     colIndex: number,
   ) => {
     if (isDragging) {
-      const newColors = localGrid.map((row, rIndex) =>
-        row.map((old_color, cIndex) =>
-          rIndex === rowIndex && cIndex === colIndex ? color : old_color,
-        ),
-      );
+      const coords = getCoords(cursorSize, [rowIndex, colIndex]);
+      const newColors = []
+      for (let row = 0; row < localGrid.length; row++) {
+        newColors.push(localGrid[row].slice());
+      }
+      for (const [x, y] of coords) {
+        newColors[x][y] = color;
+      }
+
+      setDeltas(new Set([...deltas, ...coords.map(([x, y]) => `${x},${y}`)]));
       setLocalGrid(newColors);
     }
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
   };
 
   useEffect(() => {
@@ -68,25 +99,29 @@ const Canvas: React.FC = () => {
         const newLocalGrid = Array.from({ length: currentCanvas.length }, () =>
           Array(currentCanvas[0].length).fill("0"),
         );
+
         setLocalGrid(newLocalGrid);
       }
     }
   }, [data, isLoading, error]);
 
   const handleSubmitColors = async () => {
-    const deltas = getLocalDelta(localGrid);
+    const newDeltas = Array.from(deltas).map((delta) => {
+      const [x, y] = delta.split(",").map((x) => parseInt(x));
+      return [x, y];
+    });
 
     let transactionBlock = new TransactionBlock();
-    for (const idx in deltas) {
+    for (const idx in newDeltas) {
       transactionBlock.moveCall({
         target: `0x3c5f9f43eccf2648855c6febf1143966d87541d47775b2b90c03f875da03cc71::board::update_single_pixel`,
         arguments: [
           transactionBlock.object(
             "0xe3390ae6a360a076b708691b2a1c24981b931e009cbf4aa6531e559c93d1f28c",
           ),
-          transactionBlock.pure(deltas[idx][0]),
-          transactionBlock.pure(deltas[idx][1]),
-          transactionBlock.pure(localGrid[deltas[idx][0]][deltas[idx][1]]),
+          transactionBlock.pure(newDeltas[idx][0]),
+          transactionBlock.pure(newDeltas[idx][1]),
+          transactionBlock.pure(localGrid[newDeltas[idx][0]][newDeltas[idx][1]]),
         ],
       });
     }
@@ -99,6 +134,7 @@ const Canvas: React.FC = () => {
       {
         onSuccess: (tx) => {
           console.log(tx);
+          setDeltas(new Set());
           client.waitForTransactionBlock({ digest: tx.digest }).then(() => {
             refetch();
           });
@@ -110,17 +146,17 @@ const Canvas: React.FC = () => {
     );
   };
 
-  const getLocalDelta = (localState: string[][]) => {
-    const deltaIndices: [number, number][] = [];
-    for (let row = 0; row < localState.length; row++) {
-      for (let col = 0; col < localState[row].length; col++) {
-        if (localState[row][col] !== "0") {
-          deltaIndices.push([row, col]);
-        }
-      }
-    }
-    return deltaIndices;
-  };
+  // const getLocalDelta = (localState: string[][]) => {
+  //   const deltaIndices: [number, number][] = [];
+  //   for (let row = 0; row < localState.length; row++) {
+  //     for (let col = 0; col < localState[row].length; col++) {
+  //       if (localState[row][col] !== "0") {
+  //         deltaIndices.push([row, col]);
+  //       }
+  //     }
+  //   }
+  //   return deltaIndices;
+  // };
 
   //   const getDelta = (initial: string[][], grid: string[][]) => {
   //     const deltaIndices: [number, number][] = [];
@@ -139,17 +175,36 @@ const Canvas: React.FC = () => {
   return (
     <div>
       <Container>
-        <HexColorPicker color={color} onChange={setColor} />
-        <Button onClick={handleSubmitColors}> Submit Txn </Button>
+        <HexColorPicker color={color} onChange={(color) => {
+          setColor(color);
+          setCursorSize(CursorSizes.Small);
+        }} />
+        <Flex gap="2" direction="row">
+          <Button onClick={handleSubmitColors}> Submit Txn </Button>
+          <Button onClick={() => {
+            setColor('#ffffff');
+            setCursorSize(CursorSizes.Medium);
+          }}>Eraser &#9003;</Button>
+          <RadioGroup.Root defaultValue={cursorSize} value={cursorSize} onValueChange={(value) => {
+            setCursorSize(value as CursorSizes);
+          }}>
+            <Flex gap="2" direction="column">
+              {cursorSelection.map(cursorSize => {
+                return (
+                  <Text as="label" size="2" key={cursorSize}>
+                    <Flex gap="2">
+                      <RadioGroup.Item value={cursorSize} /> {cursorSize}
+                    </Flex>
+                  </Text>
+              )})}
+            </Flex>
+          </RadioGroup.Root>
+        </Flex>
         <ColorGrid
           colors={gridColors}
-          selectedColor={color}
           localColorGrid={localGrid}
-          onColorChange={setLocalGrid}
-          onMouseDown={handleMouseDown}
+          setIsDragging={setIsDragging}
           onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-          //   selectedArea={selectedArea}
           isDragging={isDragging}
         />
       </Container>
